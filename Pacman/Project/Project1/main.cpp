7cc605d0d7c294a608f473a77f9bdab6b01b9021
AnaAlcazar <ana.alcazar@estudiantat.upc.edu>
2024-03-11 14:58:58 +0100

    Project_Update

--- Pacman/Project/Project1/main.cpp
+++ Pacman/Project/Project1/main.cpp
@@ -1,68 +1,99 @@
 #include "raylib.h"
+#include "GameManager.hpp"
 #include <iostream>
-
-Sound soundArray[10];
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
 int main(void)
 {
-    //Screen Settings
-    int screenWidth = 224;
-    int screenHeight = 256;
-    
-
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
-    Texture2D pacmanSpriteSheet = LoadTexture("../Assets/Pacmanspritesheet.png");
+#pragma region Variables
+    Sound soundArray[10];
+    float screenWidth = 224;
+    float screenHeight = 256;
+#pragma endregion
+#pragma region Awake
     InitAudioDevice();
-
-    soundArray[0] = LoadSound("resources/raylib_audio_resources/sound.wav");
-
-    Music music = LoadMusicStream("resources/raylib_audio_resources/country.mp3");
-
-    music.looping = true;
-    float pitch = 0.5f;
-
-    PlayMusicStream(music);
-    
-
-    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
-    //--------------------------------------------------------------------------------------
-
-    // Main game loop
-    static double x = 120, y = 35;
-    static double speed_x = 2, speed_y = 2;
-
+#pragma endregion
+#pragma region Start
+    SetTargetFPS(60);
+#pragma endregion
+#pragma region Loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        // TODO: Update your variables here
-        if (IsKeyDown(KEY_RIGHT)) x += speed_x;
-        if (IsKeyDown(KEY_LEFT)) x -= speed_x;
-        if (IsKeyDown(KEY_DOWN)) y += speed_y;
-        if (IsKeyDown(KEY_UP)) y -= speed_y;
+#pragma region Input
 
-        if (IsKeyPressed(KEY_SPACE))
-            PlaySound(soundArray[0]);
-        //----------------------------------------------------------------------------------
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing(); //Prepares next drawing
+#pragma endregion
 
-        ClearBackground(RAYWHITE);
+#pragma region Early Update
 
-        EndDrawing(); //Ends drawing + updates inputs
-        //----------------------------------------------------------------------------------
+#pragma endregion
+
+#pragma region Update
+
+#pragma endregion
+
+#pragma region Late Update
+
+#pragma endregion
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseAudioDevice();
-    CloseWindow();        // Close window and OpenGL context
-    //--------------------------------------------------------------------------------------
+#pragma endregion
+#pragma region Render
 
-    return 0;
+#pragma endregion
+
+
+
+
+    
+    
+    //InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+    
+
+    //soundArray[0] = LoadSound("resources/raylib_audio_resources/sound.wav");
+
+    //Music music = LoadMusicStream("resources/raylib_audio_resources/country.mp3");
+
+    //music.looping = true;
+    //float pitch = 0.5f;
+
+    //PlayMusicStream(music);
+    
+
+                   // Set our game to run at 60 frames-per-second
+    ////--------------------------------------------------------------------------------------
+
+    //// Main game loop
+
+    //while (!WindowShouldClose())    // Detect window close button or ESC key
+    //{
+    //    // Update
+    //    //----------------------------------------------------------------------------------
+    //    // TODO: Update your variables here
+    //    if (IsKeyDown(KEY_RIGHT)) x += speed_x;
+    //    if (IsKeyDown(KEY_LEFT)) x -= speed_x;
+    //    if (IsKeyDown(KEY_DOWN)) y += speed_y;
+    //    if (IsKeyDown(KEY_UP)) y -= speed_y;
+
+    //    if (IsKeyPressed(KEY_SPACE))
+    //        PlaySound(soundArray[0]);
+    //    //----------------------------------------------------------------------------------
+    //    // Draw
+    //    //----------------------------------------------------------------------------------
+    //    BeginDrawing(); //Prepares next drawing
+
+    //    ClearBackground(RAYWHITE);
+
+    //    EndDrawing(); //Ends drawing + updates inputs
+    //    //----------------------------------------------------------------------------------
+    //}
+
+    //// De-Initialization
+    ////--------------------------------------------------------------------------------------
+    //CloseAudioDevice();
+    //CloseWindow();        // Close window and OpenGL context
+    ////--------------------------------------------------------------------------------------
+
+    //return 0;
 }
-// raylib example source code
