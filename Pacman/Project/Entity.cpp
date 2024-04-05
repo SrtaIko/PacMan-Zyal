f424a17d8722fdec5677354993af65583731ff51
Biker13 <iker.salas@estudiantat.upc.edu>
2024-04-05 12:25:43 +0200

    Make all the documents

---
+++ Pacman/Project/Entity.cpp
@@ -0,0 +1,12 @@
+#include "Entity.h"
+
+
+Entity::Entity()
+    {
+        pos = { 0, 0 };
+        dir = { 0, 0 };
+        width = 0;
+        height = 0;
+        speed = 0;
+        is_alive = false;
+    }
---
+++ Pacman/Project/Entity.h
@@ -0,0 +1,14 @@
+#pragma once
+
+
+class Entity {
+public: 
+
+
+
+private:
+    Vector2 pos, dir;
+    int width, height;
+    int speed;
+    bool is_alive;
+};
